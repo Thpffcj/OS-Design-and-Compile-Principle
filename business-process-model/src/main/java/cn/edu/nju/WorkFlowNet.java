1b9e69d743c7a0570b6479f0e26f662b0d520c98
Thpffcj <1441732331@qq.com>
2019-11-09 18:43:43 +0800

    高级算法大作业

---
+++ business-process-model/src/main/java/cn/edu/nju/WorkFlowNet.java
@@ -0,0 +1,19 @@
+package cn.edu.nju;
+
+/**
+ * Created by thpffcj on 2019/11/9.
+ */
+public class WorkFlowNet {
+
+    public static void getLogOfModel(String modelFile, String logFile) {
+
+    }
+
+    public static void main(String[] args) {
+
+        String modelFile = "src/resources/Model1.pnml";
+        String logFile = "src/resources/output";
+
+        getLogOfModel(modelFile, logFile);
+    }
+}
