d76ca7bbf3f449a8296e2cc3433bb0a7dfb08559
Thpffcj <1441732331@qq.com>
2019-11-21 14:12:31 +0800

    更新readme

--- business-process-model/src/main/java/cn/edu/nju/WorkFlowNet.java
+++ business-process-model/src/main/java/cn/edu/nju/WorkFlowNet.java
@@ -1,5 +1,11 @@
 package cn.edu.nju;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 /**
  * Created by thpffcj on 2019/11/9.
  */
@@ -7,12 +13,23 @@ public class WorkFlowNet {
 
     public static void getLogOfModel(String modelFile, String logFile) {
 
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(modelFile);
+
+            NodeList placeList = document.getElementsByTagName("place");
+            System.out.println(placeList.getLength());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
 
-        String modelFile = "src/resources/Model1.pnml";
-        String logFile = "src/resources/output";
+        String modelFile = "src/main/resources/Model1.pnml";
+        String logFile = "src/main/resources/output";
 
         getLogOfModel(modelFile, logFile);
     }
